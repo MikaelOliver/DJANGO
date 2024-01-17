ae364b756fbb5a931758362027a826975845de95
Mikael Oliveira <mikaeloliver0987@gmail.com>
2024-01-17 15:44:22 -0300

    arquivos do projeto 10

---
+++ decimo_projeto/home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Produto
+
+class ProdutoList(admin.ModelAdmin):
+    list_display = ('nome_produto','preco','imagem_produto','descricao',)
+
+
+admin.site.register(Produto,ProdutoList)
---
+++ decimo_projeto/home/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .forms import ContatoForm, ProdutoModelForm
+from django.contrib import messages
+from django.http import HttpResponse
+
+def home(request):
+     return render(request,'home/index.html')
+
+def contato(request):
+     form = ContatoForm(request.POST)
+
+     if str(request.method) == 'POST':
+          if form.is_valid():
+               nome = form.cleaned_data['nome']
+               email = form.cleaned_data['email']
+               assunto = form.cleaned_data['assunto']
+               mensagem = form.cleaned_data['mensagem']
+
+               print('Mensagem enviada')
+               print(f'Nome: {nome}')
+               print(f'E-mail: {email}')
+               print(f'Assunto: {assunto}')
+               print(f'Mensagem: {mensagem}')
+
+               messages.success(request, 'E-mail enviado com sucesso!')
+               form = ContatoForm()
+          else:
+               messages.error(request,'Erro ao enviar seu E-mail!')
+
+     context = {
+          'form' : form
+     }
+     return render(request,'home/contato.html',context)
+
+def produto(request):
+     if str(request.method) == 'POST':
+          form = ProdutoModelForm(request.POST,request.FILES)
+          if form.is_valid():
+               form.save()
+               messages.success(request,'Produto cadastrado com sucesso!')
+          else:
+               messages.error(request,'Error ao cadastrar produto!')
+     else:
+          form = ProdutoModelForm()
+
+     context = {
+          'form' : form
+     }
+
+     return render(request,'home/produto.html',context)
